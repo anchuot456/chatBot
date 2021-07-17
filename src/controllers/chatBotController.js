eefb5451b8360f6665f35c6f863b4e55451a0f25
anchuot456 <37763345+anchuot456@users.noreply.github.com>
2021-07-17 16:57:29 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import resquest from "request";
+import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
