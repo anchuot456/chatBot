ac1781cd6de91813d4c7b93ece30cfda94346371
anchuot456 <37763345+anchuot456@users.noreply.github.com>
2021-07-22 13:35:16 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -143,8 +143,7 @@ async function handlePostback(sender_psid, received_postback) {
   let response;
   console.log(received_postback.payload);
   // Get the payload for the postback
-  let payload = JSON.parse(received_postback.payload);
-  console.log(payload);
+  let payload = received_postback.payload;
 
   // Set the response based on the postback payload
   switch (payload) {
